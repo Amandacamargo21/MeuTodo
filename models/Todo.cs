e96c1d318ed354ec6d43900bb650f115db129904
Amandacamargo21 <amandacamargo2102@gmail.com>
2022-10-05 20:46:42 -0300

    Initial

---
+++ models/Todo.cs
@@ -0,0 +1,14 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MeuTodo.Models
+{
+    public class Todo
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public bool Done { get; set; }
+        public DateTime Date { get; set; } = DateTime.Now;
+    }
+}
